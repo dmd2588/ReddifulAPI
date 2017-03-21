29d9f5661a805ece4e056f30d225382b1c433b32
phalax4 <jeremylim995@gmail.com>
2017-03-21 18:23:17 -0500

    Forgot comments jsx

---
+++ www/components/Comments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+var Comments = React.createClass({
+        render: function() {
+          return (
+            <div><h2>Comments</h2></div>
+          )
+        }
+});
+
+export default Comments;
