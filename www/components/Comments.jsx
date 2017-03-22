33343e548974c94d52ed94eb5416c5d5236c429b
Austin Middleton <xuset1@gmail.com>
2017-03-22 18:41:25 -0500

    Add customizable grid react component

--- www/components/Comments.jsx
+++ www/components/Comments.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import MyGrid from './MyGrid.jsx';
+import RfGrid from './RfGrid.jsx'
 
-var Comments = React.createClass({
-        render: function() {
-          return (
-            <MyGrid/>
-          )
-        }
-});
+class Comments extends React.Component {
+  constructor() {
+    super()
+    this.cells = [
+      {title: 'comment 1', subtitle: 'by: blue whale', text: 'Dolphins ruin the sanctity of sea life!'},
+      {title: 'comment 2', subtitle: 'by: red dolphin', text: 'whales are an abomination.'}
+    ]
+  }
+
+  render() {
+    return (
+      <RfGrid cells={this.cells}/>
+    )
+  }
+}
 
 export default Comments;
