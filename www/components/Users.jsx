54f8b1a649bf6e79edbbd995d97bd35cc7285609
Thomas Lam <t.lam@lamsinfosystem.com>
2017-03-23 01:44:54 -0500

    Users Grid Using Sample Data

--- www/components/Users.jsx
+++ www/components/Users.jsx
@@ -3,7 +3,14 @@ import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox
 import { Button } from 'react-toolbox/lib/button';
 import { Grid, Row, Col } from 'react-bootstrap';
 
+var moment = require('moment');
+var userData = [{"name": "batman_jr", "created": 1405828618.0, "link_karma": 335, "comment_karma": 190, "id": "hh8mr", "email": "None"}, {"name": "poizan42", "created": 1331171212.0, "link_karma": 768, "comment_karma": 25419, "id": "74344", "email": "poizan@poizan.dk"}, {"name": "Ooer", "created": 1287044616.0, "link_karma": 18834, "comment_karma": 56852, "id": "4fer6", "email": "ooer@live.com"}];
+
 export default function Users () {
+  var dates = [new Date(0), new Date(0), new Date(0)];
+  for (var i = 0; i < 3; i++) {
+    dates[i].setUTCSeconds(userData[i].created);
+  }
   return (
     <div>
       <h2>Users</h2>
@@ -13,15 +20,15 @@ export default function Users () {
             <Card style={{width: '250px'}}>
               <CardTitle
                 avatar="https://placeimg.com/80/80/animals"
-                title="Bob the Dwarf Warrior"
-                subtitle="Joined: Thr July 12, 2007"
+                title={userData[0].name}
+                subtitle={"Joined: " + moment(dates[0]).format("LL")}
               />
               <CardMedia
                 aspectRatio="wide"
                 image="https://placeimg.com/800/450/nature"
               />
               <CardActions>
-                <Button label="Details" />
+                <Button href={"/users/detail/" + userData[0].id} label="Details" />
               </CardActions>
             </Card>
           </Col>
@@ -29,15 +36,15 @@ export default function Users () {
             <Card style={{width: '250px'}}>
               <CardTitle
                 avatar="https://placeimg.com/80/80/animals"
-                title="1337H4X0rElf"
-                subtitle="Joined: Fri Jan 9, 2006"
+                title={userData[1].name}
+                subtitle={"Joined: " + moment(dates[1]).format("LL")}
               />
               <CardMedia
                 aspectRatio="wide"
                 image="https://placeimg.com/800/450/nature"
               />
               <CardActions>
-                <Button label="Details" />
+                <Button href={"/users/detail/" + userData[1].id} label="Details" />
               </CardActions>
             </Card>
           </Col>
@@ -45,15 +52,15 @@ export default function Users () {
             <Card style={{width: '250px'}}>
               <CardTitle
                 avatar="https://placeimg.com/80/80/animals"
-                title="GlobinBossHelga"
-                subtitle="Joined: Wed May 3, 2103"
+                title={userData[2].name}
+                subtitle={"Joined: " + moment(dates[2]).format("LL")}
               />
               <CardMedia
                 aspectRatio="wide"
                 image="https://placeimg.com/800/450/nature"
               />
               <CardActions>
-                <Button label="Details" />
+                <Button href={"/users/detail/" + userData[2].id} label="Details" />
               </CardActions>
             </Card>
           </Col>
