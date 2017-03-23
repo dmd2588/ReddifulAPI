7b827e6015ff15c8a5232cca07b040425bb29646
Austin Middleton <xuset1@gmail.com>
2017-03-23 17:36:52 -0500

    Add ability to link content in Details

--- www/components/Details.jsx
+++ www/components/Details.jsx
@@ -4,9 +4,15 @@ export default function Details (props) {
   return (
     <div>
       <h2>{props.title}</h2>
-      {Object.keys(props.details).map(title => (
-          <p key={Math.random().toString(16).substr(2)}>{title}: {props.details[title]}</p>
-      ))}
+      {Object.keys(props.details).map(title => {
+        const key = Math.random().toString(16).substr(2)
+        let detail = props.details[title]
+        let inner = typeof detail !== 'object' ? detail
+                    : <a href={detail.link}>{detail.name}</a>
+        return (
+          <p key={key}>{title}: {inner}</p>
+        )
+      })}
     </div>
   )
 }
