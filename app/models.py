06a4e95d7c047c5f5df46c62c04e1e8e89482fd4
Johnny Knoebel <Johnny Knoebel>
2017-03-23 17:33:33 -0500

    passing ppylint and update makefile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+# pylint: disable = too-many-instance-attributes
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
@@ -5,14 +12,19 @@ from sqlalchemy.sql.schema import Table
 
 Base = declarative_base()
 
+#association table for the many-to-many relationship between User and Subreddit
 user_sub_table = Table('users_subs', Base.metadata,
                        Column('user_id', Integer, ForeignKey('users.id')),
                        Column('sub_id', Integer, ForeignKey('subreddits.id')))
 
+# ------------
+# User
+# ------------
+
 class User(Base):
     __tablename__ = 'users'
-        
-    id = Column(String, primary_key=True)
+
+    user_id = Column(String, primary_key=True)
     name = Column(String)
     link_karma = Column(Integer)
     comment_karma = Column(Integer)
@@ -21,38 +33,70 @@ class User(Base):
     posts = relationship("Post", back_populates="users")
     comments = relationship("Comment", back_populates="users")
     subreddits = relationship("Subreddit", secondary=user_sub_table)
-    
-    def __init__(self, id, name, link_karma, comment_karma, created, email):
-        self.id = id
-        self.name = name
-        self.link_karma = link_karma
-        self.comment_karma = comment_karma
-        self.created = created
-        self.email = email
-        
-    
+
+    def __init__(self, **attr):
+        """
+        creates a User with given attributes
+        """
+
+        assert attr["id"] is str
+        assert attr["name"] is str
+        assert attr["link_karma"] >= 0 and attr["link_karma"] is int
+        assert attr["comment_karma"] >= 0 and attr["comment_karma"] is int
+        assert attr["created"] >= 0 and attr["created"] is int
+        assert attr["email"] is str
+        self.user_id = attr["id"]
+        self.name = attr["name"]
+        self.link_karma = attr["link_karma"]
+        self.comment_karma = attr["comment_karma"]
+        self.created = attr["created"]
+        self.email = attr["email"]
+
+
     def getID(self):
-        return id
-    
+        """
+        returns ID of the user
+        """
+        return self.user_id
+
     def getName(self):
-        return name
-    
+        """
+        returns name of the user
+        """
+        return self.name
+
     def getLKarma(self):
-        return link_karma
-    
+        """
+        returns link karma of the user
+        """
+        return self.link_karma
+
     def getCKarma(self):
-        return comment_karma
-    
+        """
+        returns comment karma of the user
+        """
+        return self.comment_karma
+
     def getAge(self):
-        return created
-    
+        """
+        returns time created of the user
+        """
+        return self.created
+
     def getEmail(self):
-        return email
-    
+        """
+        returns email of the user
+        """
+        return self.email
+
+# ------------
+# Subreddit
+# ------------
+
 class Subreddit(Base):
     __tablename__ = 'subreddits'
-    
-    id = Column(String, primary_key=True)
+
+    sub_id = Column(String, primary_key=True)
     display_name = Column(String)
     subscribers = Column(Integer)
     accounts_active = Column(Integer)
@@ -61,37 +105,69 @@ class Subreddit(Base):
     posts = relationship("Post", back_populates="subreddits")
     comments = relationship("Comments", back_populates="subreddits")
     users = relationship("User", secondary=user_sub_table)
-    
-    def __init__(self, id, display_name, subscribers, accounts_active, created, title):
-        self.id = id
-        self.display_name = display_name
-        self.subscribers = subscribers
-        self.accounts_active = accounts_active
-        self.created = created
-        self.title = title
-    
+
+    def __init__(self, **attr):
+        """
+        creates a Subreddit with given attributes
+        """
+        assert attr["id"] is str
+        assert attr["display_name"] is str
+        assert attr["subscribers"] >= 0 and attr["subscribers"] is int
+        assert attr["accounts_active"] >= 0 and attr["accounts_active"] is int
+        assert attr["created"] >= 0 and attr["created"] is int
+        assert attr["title"] is str
+
+        self.sub_id = attr["id"]
+        self.display_name = attr["display_name"]
+        self.subscribers = attr["subscribers"]
+        self.accounts_active = attr["accounts_active"]
+        self.created = attr["created"]
+        self.title = attr["title"]
+
     def getID(self):
-        return id
-    
+        """
+        returns ID of the subreddit
+        """
+        return self.sub_id
+
     def getName(self):
-        return display_name
-    
+        """
+        returns name of the subreddit
+        """
+        return self.display_name
+
     def getSubscribers(self):
-        return subscribers
-    
+        """
+        returns number of subscribers of the subreddit
+        """
+        return self.subscribers
+
     def getVisitors(self):
-        return accounts_active
-    
+        """
+        returns number of current visitors of the subreddit
+        """
+        return self.accounts_active
+
     def getTitle(self):
-        return title
-    
+        """
+        returns the title of the subreddit
+        """
+        return self.title
+
     def getAge(self):
-        return created
-    
-class Post(Base):    
+        """
+        returns the time created of the subreddit
+        """
+        return self.created
+
+# ------------
+# Post
+# ------------
+
+class Post(Base):
     __tablename__ = 'posts'
-    
-    id = Column(String, primary_key=True)
+
+    post_id = Column(String, primary_key=True)
     title = Column(String)
     url = Column(String)
     score = Column(Integer)
@@ -105,62 +181,109 @@ class Post(Base):
     sub = relationship("Subreddit", back_populates="posts")
     author_id = Column(String, ForeignKey('users.id'))
     author = relationship("User", back_populates="posts")
-    
-    def __init__(self, id, title, url, score, created, nsfw, self_post, self_text, created, gilded, subreddit_id, author_id):
-        self.id = id
-        self.title = title
-        self.url = url
-        self.score = score
-        self.created = created
-        self.nsfw = nsfw
-        self.self_post = self_post
-        self.self_text = self_text
-        self.created = created
-        self.gilded = gilded
-        self.subreddit_id = subreddit_id
-        self.author_id = author_id
-        
-    
+
+    def __init__(self, **attr):
+        """
+        creates a Post with the given attributes
+        """
+        assert attr["id"] is str
+        assert attr["title"] is str
+        assert attr["url"] is str
+        assert attr["score"] >= 0 and attr["score"] is int
+        assert attr["created"] >= 0 and attr["created"] is int
+        assert attr["nsfw"] is bool
+        assert attr["self_post"] is bool
+        assert attr["self_text"]is str
+        assert attr["gilded"] >= 0 and attr["gilded"] is int
+        assert attr["subreddit_id"] is str
+        assert attr["author_id"] is str
+
+        self.post_id = attr["id"]
+        self.title = attr["title"]
+        self.url = attr["url"]
+        self.score = attr["score"]
+        self.created = attr["created"]
+        self.nsfw = attr["nsfw"]
+        self.self_post = attr["self_post"]
+        self.self_text = attr["self_text"]
+        self.gilded = attr["gilded"]
+        self.subreddit_id = attr["subreddit_id"]
+        self.author_id = attr["author_id"]
+
     def getID(self):
-        return id
-    
+        """
+        returns ID of the post
+        """
+        return self.post_id
+
     def getTitle(self):
-        return title
-    
+        """
+        returns title of the post
+        """
+        return self.title
+
     def getURL(self):
-        return url
-    
+        """
+        returns URL of the post
+        """
+        return self.url
+
     def getKarma(self):
-        return score
-    
-    def getAuthor(self):
-        return author_id
-    
+        """
+        returns karma of the post
+        """
+        return self.score
+
     def isNSFW(self):
-        return nsfw
-    
+        """
+        returns if the post is NSFW
+        """
+        return self.nsfw
+
     def isText(self):
-        return self_post
-    
+        """
+        returns if the post is a self-post (text only)
+        """
+        return self.self_post
+
     def getText(self):
-        return self_text
-    
+        """
+        returns the text of the post if it is a self-post
+        """
+        return self.self_text
+
     def getTime(self):
-        return created
-    
+        """
+        returns the time created of the post
+        """
+        return self.created
+
     def getGilded(self):
-        return gilded
-    
+        """
+        returns how many times the post was gilded
+        """
+        return self.gilded
+
     def getSubredditID(self):
-        return subreddit_id
-    
+        """
+        returns ID of the post's subreddit
+        """
+        return self.subreddit_id
+
     def getAuthorID(self):
-        return author_id
+        """
+        returns ID of the post
+        """
+        return self.author_id
+
+# ------------
+# Comment
+# ------------
 
 class Comment(Base):
     __tablename__ = 'comments'
-        
-    id = Column(String, primary_key=True)
+
+    comment_id = Column(String, primary_key=True)
     body = Column(String)
     score = Column(Integer)
     created = Column(String)
@@ -172,42 +295,82 @@ class Comment(Base):
     author = relationship("User", back_populates="comments")
     subreddit_id = Column(String, ForeignKey('subreddits.id'))
     sub = relationship("Subreddit", back_populates="comments")
-    
-    def __init__(self, id, body, score, created, gilded, edited, link_id, subreddit_id, author_id):
-        self.id = id
-        self.body = body
-        self.score = score
-        self.created = created
-        self.gilded = gilded
-        self.edited = edited
-        self.link_id = link_id
-        self.subreddit_id = subreddit_id
-        self.author_id = author_id
-    
+
+    def __init__(self, **attr):
+        """
+        creates a Comment with the given attributes
+        """
+        assert attr["id"] is str
+        assert attr["body"] is str
+        assert attr["score"] >= 0 and attr["score"] is int
+        assert attr["created"] >= 0 and attr["created"] is int
+        assert attr["edited"] is bool
+        assert attr["gilded"] >= 0 and attr["gilded"] is int
+        assert attr["link_id"] is str
+        assert attr["subreddit_id"] is str
+        assert attr["author_id"] is str
+
+        self.comment_id = attr["id"]
+        self.body = attr["body"]
+        self.score = attr["score"]
+        self.created = attr["created"]
+        self.gilded = attr["gilded"]
+        self.edited = attr["edited"]
+        self.link_id = attr["link_id"]
+        self.subreddit_id = attr["subreddit_id"]
+        self.author_id = attr["author_id"]
+
     def getID(self):
-        return id
-    
+        """
+        returns ID of the comment
+        """
+        return self.comment_id
+
     def getText(self):
-        return body
-    
+        """
+        returns the text of the comment
+        """
+        return self.body
+
     def getKarma(self):
-        return score
-    
+        """
+        returns karma of the comment
+        """
+        return self.score
+
     def getTime(self):
-        return created
-        
+        """
+        returns time created of the comment
+        """
+        return self.created
+
     def getGilded(self):
-        return gilded
-    
+        """
+        returns how many times the comment was gilded
+        """
+        return self.gilded
+
     def isEdited(self):
-        return edited
-    
+        """
+        returns if the comment was edited
+        """
+        return self.edited
+
     def getLinkID(self):
-        return link_id
-    
+        """
+        returns ID of the comment's post
+        """
+        return self.link_id
+
     def getAuthorID(self):
-        return user_id
-    
+        """
+        returns ID of the comment's author
+        """
+        return self.user_id
+
     def getSubredditID(self):
-        return subreddit_id
+        """
+        returns ID of the comment's subreddit
+        """
+        return self.subreddit_id
  
--- makefile
+++ makefile
@@ -1,5 +1,43 @@
 .DEFAULT_GOAL := build
 
+ifeq ($(shell uname), Darwin)          # Apple
+    PYTHON   := python3.5
+    PIP      := pip3.5
+    PYLINT   := pylint
+    COVERAGE := coverage-3.5
+    PYDOC    := pydoc3.5
+    AUTOPEP8 := autopep8
+else ifeq ($(CI), true)                # Travis CI
+    PYTHON   := python3.5
+    PIP      := pip3.5
+    PYLINT   := pylint
+    COVERAGE := coverage-3.5
+    PYDOC    := pydoc3.5
+    AUTOPEP8 := autopep8
+else ifeq ($(shell uname -p), unknown) # Docker
+    PYTHON   := python3.5
+    PIP      := pip3.5
+    PYLINT   := pylint
+    COVERAGE := coverage-3.5
+    PYDOC    := pydoc3.5
+    AUTOPEP8 := autopep8
+else                                   # UTCS
+    PYTHON   := python3
+    PIP      := pip3
+    PYLINT   := pylint3
+    COVERAGE := coverage-3.5
+    PYDOC    := pydoc3.5
+    AUTOPEP8 := autopep8
+endif
+
+.pylintrc:
+	$(PYLINT) --disable=locally-disabled --reports=no --generate-rcfile > $@
+	
+IDB1.html: app/models.py
+	pydoc3 -w app/models.py
+	cp models.html IDB1.html
+	rm models.html
+
 config:
 	git config -l
 
